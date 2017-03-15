2753c30a21e4ee7880533acc43389f6cb7e186f2
Qing Xia <qixia@sonicwall.com>
2017-03-15 18:25:41 +0800

    add curl.py

---
+++ tsa_scalability/curl.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import pycurl
+import StringIO
+
+buf = StringIO.StringIO()
+
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://10.103.64.44')
+c.setopt(c.PORT, 12348)
+c.setopt(c.POSTFIELDS, '{"MethodInput":"","MethodName":"GetApplianceList","Stub":2}')
+c.setopt(c.HTTPHEADER, ['Content-Type: application/json'])
+#c.setopt(c.VERBOSE, True)
+c.setopt(c.WRITEFUNCTION, buf.write)
+c.perform()
+
+result = buf.getvalue()
+print(result)
+buf.close()
