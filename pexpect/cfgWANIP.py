3bafb82b9253c44e32ed768d7c753b87667b3f88
Qing Xia <qixia@sonicwall.com>
2017-03-21 12:10:19 +0800

    move into functions

--- pexpect/cfgWANIP.py
+++ pexpect/cfgWANIP.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-'''This script could automatically configure WAN IP after UTM is factory default reset.
+'''
+This script could automatically configure WAN IP after UTM is factory default reset.
 Written by: Alex (Qing) Xia
-Version: 0.1
-Date: Oct 17th 2016
+Version: 0.2
+Date: 3/21/2017
 This script doesn't work if UTM is not in non-conf t mode.
 '''
 
@@ -21,82 +22,88 @@ ipDict = {'10.0.0.66':'2029',
 '10.0.0.67':'2026',
 '10.0.0.68':'2027'}
 
-usage = '''Usage: command WANIP
+def usage():
+    print '''Usage: command WANIP
 e.g. ./cfgWANIP.py 10.0.0.25'''
 
-while True:
-    if len(sys.argv) == 1:
-        print usage
-        sys.exit()
-    elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
-        print usage
-        sys.exit()
-    elif len(sys.argv) != 2:
-        print usage
-        sys.exit()
-    else:
-        break
+def checkPara():
+    while True:
+        if len(sys.argv) == 1:
+            usage()
+            sys.exit()
+        elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            usage()
+            sys.exit()
+        elif len(sys.argv) != 2:
+            usage()
+            sys.exit()
+        else:
+            break
 
-# telnet login (console login)
-wanip = sys.argv[1]
-port = ipDict[wanip]
+def cfgWANIP(wanip):
+    # telnet login (console login)
+    port = ipDict[wanip]
+    
+    # start to telnet
+    child = pexpect.spawn("telnet 10.103.64.8 %s" % port)
+    child.logfile = sys.stdout
+    child.expect("login:")
+    child.sendline("admin")
+    child.expect("Password:")
+    child.sendline("password")
+    time.sleep(1)
+    child.sendline("")
+    
+    while True:
+        index = child.expect(["User", "admin@[A-Z0-9]{12}>"])
+        if index == 0:
+            child.sendline("admin")
+            child.expect("Password:")
+            child.sendline("password")
+            child.expect("admin@[A-Z0-9]{12}>")
+            child.sendline("configure terminal")
+            break
+        elif index == 1:
+            child.sendline("configure terminal")        
+            break
+        else:
+            print "Program error! Exit."
+            sys.exit()
+    
+    # login to UTM console, starting to configure WAN IP
+    child.expect("config\([A-Z0-9]{12}\)#")
+    child.sendline("interface X1")
+    
+    child.expect("\(edit-interface\[X1\]\)#")
+    child.sendline("ip-assignment WAN static")
+    
+    child.expect("\(edit-WAN-static\[X1\]\)#")
+    child.sendline("ip %s netmask 255.255.255.0" % wanip)
+    
+    child.expect("\(edit-WAN-static\[X1\]\)#")
+    child.sendline("gateway 10.0.0.1")
+    
+    child.expect("\(edit-WAN-static\[X1\]\)#")
+    child.sendline("dns primary 10.217.131.101")
+    
+    child.expect("\(edit-WAN-static\[X1\]\)#")
+    child.sendline("exit")
+    
+    child.expect("\(edit-interface\[X1\]\)#")
+    child.sendline("management https")
+    
+    child.expect("\(edit-interface\[X1\]\)#")
+    child.sendline("management ssh")
+    
+    child.expect("\(edit-interface\[X1\]\)#")
+    child.sendline("management ping")
+    
+    child.expect("\(edit-interface\[X1\]\)#")
+    child.sendline("commit")
+    
+    time.sleep(2)
+    child.close()
 
-# start to telnet
-child = pexpect.spawn("telnet 10.103.64.8 %s" % port)
-child.logfile = sys.stdout
-child.expect("login:")
-child.sendline("admin")
-child.expect("Password:")
-child.sendline("password")
-time.sleep(1)
-child.sendline("")
-
-while True:
-    index = child.expect(["User", "admin@[A-Z0-9]{12}>"])
-    if index == 0:
-        child.sendline("admin")
-        child.expect("Password:")
-        child.sendline("password")
-        child.expect("admin@[A-Z0-9]{12}>")
-        child.sendline("configure terminal")
-        break
-    elif index == 1:
-        child.sendline("configure terminal")        
-        break
-    else:
-        print "Program error! Exit."
-        sys.exit()
-
-# login to UTM console, starting to configure WAN IP
-child.expect("config\([A-Z0-9]{12}\)#")
-child.sendline("interface X1")
-
-child.expect("\(edit-interface\[X1\]\)#")
-child.sendline("ip-assignment WAN static")
-
-child.expect("\(edit-WAN-static\[X1\]\)#")
-child.sendline("ip %s netmask 255.255.255.0" % wanip)
-
-child.expect("\(edit-WAN-static\[X1\]\)#")
-child.sendline("gateway 10.0.0.1")
-
-child.expect("\(edit-WAN-static\[X1\]\)#")
-child.sendline("dns primary 10.217.131.101")
-
-child.expect("\(edit-WAN-static\[X1\]\)#")
-child.sendline("exit")
-
-child.expect("\(edit-interface\[X1\]\)#")
-child.sendline("management https")
-
-child.expect("\(edit-interface\[X1\]\)#")
-child.sendline("management ssh")
-
-child.expect("\(edit-interface\[X1\]\)#")
-child.sendline("management ping")
-
-child.expect("\(edit-interface\[X1\]\)#")
-child.sendline("commit")
-
-time.sleep(2)
-child.close()
+if __name__ == '__main__':
+    checkPara()
+    cfgWANIP(wanip = sys.argv[1])
