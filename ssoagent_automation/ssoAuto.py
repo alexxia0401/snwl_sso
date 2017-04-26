b36f22c3154d10c7955c89f602bffb6182501adf
Qing Xia <qixia@sonicwall.com>
2017-04-26 10:39:18 +0800

    completed class RemoteSSO

--- ssoagent_automation/ssoAuto.py
+++ ssoagent_automation/ssoAuto.py
@@ -5,13 +5,12 @@ This script uses JSON to communicate with SSO agent to configure SSO appliance.
 Apply for SSO agent 4.1.x
 Tested under Python 3.5.x
 Author: Qing Xia
-Create Date: April 11th 2017
 '''
 
-import urllib.request
 import json
+import urllib.request
 
-class SSO:
+class SSO(object):
     '''Implemented add, get, delete SSO agent'''
     header = {'Content-Type': 'application/json'}
 
@@ -52,7 +51,7 @@ class SSO:
         SSO.addSSOData['MethodInput']['Port'] = port
         SSO.addSSOData['MethodInput']['FriendlyName'] = str(name)
         SSO.addSSOData['MethodInput']['SharedKey'] = str(key)
-        #print SSO.addSSOData
+        
         jdata = json.dumps(SSO.addSSOData)
         req = urllib.request.Request(self.url,
                                      jdata.encode('utf-8'),
@@ -61,7 +60,7 @@ class SSO:
         data = response.read()
         response.close()
         data = data.decode()
-        #print data
+        #print(data)
         data = json.loads(data)
         if data['Message'] == 'success':
             return (True, data)
@@ -86,7 +85,7 @@ class SSO:
         for i in data['MethodOutput']:
             print('IP:', i['Ip'])
             print('SharedKey:', i['SharedKey'])
-            print('FriendName:', i['FriendlyName'])
+            print('FriendlyName:', i['FriendlyName'])
             print('-' * 20)
 
     def updSSO(self):
@@ -101,13 +100,12 @@ class SSO:
         data = response.read()
         response.close()
         data = data.decode()
-        #print data
+        #print(data)
         data = json.loads(data)
 
         for i in data['MethodOutput']:
             if i['Ip'] == str(ip):
                 index = i['Index']
-        #print index
         try:
             SSO.delSSOData['MethodInput']['Index'] = index
         except:
@@ -122,7 +120,7 @@ class SSO:
             data = response.read()
             response.close()
             data = data.decode()
-            #print data
+            #print(data)
             data = json.loads(data)
             if data['Message'] == 'success':
                 return (True, data)
@@ -130,17 +128,129 @@ class SSO:
                 return (False, data)
             #print('Deleting SSO agent %s result: %s' % (ip, data['Message']))
 
-if __name__ == '__main__':
-    alexSSO = SSO('http://10.103.64.44:12348')
-    result = alexSSO.getSSO()
-    alexSSO.showSSO(result)
-    (result, data) = alexSSO.addSSO('6.6.7.8', '123456', 'test7')
-    if result == True:
-        print('adding successfully.')
-        #print(data)
-    alexSSO.showSSO(alexSSO.getSSO())
+class RemoteSSO(object):
+    '''Add, Delete, Get Remote SSO Agents'''
+    header = {'Content-Type': 'application/json'}
 
-    (result, data) = alexSSO.delSSO('6.6.7.8')
-    if result == 1:
-        print('deleting successfully.')
-    alexSSO.showSSO(alexSSO.getSSO())
+    addData = {
+        "MethodInput": {
+            "FriendlyName": "",
+            "Index": 0,
+            "Ip": "",
+            "Port": -1
+        },
+        "MethodName": "AddRemoteAgent",
+        "Stub": 1
+    }
+ 
+    getData = {
+        "MethodInput": "",
+        "MethodName": "GetRemoteAgentList",
+        "Stub": 2
+    }
+
+    delData = {
+        "MethodInput": {
+            "Index": -1
+        },
+        "MethodName": "RemoveRemoteAgent",
+        "Stub": 3
+    }
+ 
+    def __init__(self, url):
+        self.url = url
+
+    def addRemoteSSO(self, ip, port, name):
+        RemoteSSO.addData['MethodInput']['Ip'] = str(ip)
+        RemoteSSO.addData['MethodInput']['Port'] = port
+        RemoteSSO.addData['MethodInput']['FriendlyName'] = str(name)
+        jdata = json.dumps(RemoteSSO.addData)
+
+        req = urllib.request.Request(self.url,
+                                     jdata.encode('utf-8'),
+                                     RemoteSSO.header)
+        response = urllib.request.urlopen(req)
+        data = response.read()
+        response.close()
+        data = json.loads(data.decode())
+        #print(data)
+        if data['Message'] == 'success':
+            return (True, data)
+        else:
+            return (False, data)
+        
+    def getRemoteSSO(self):
+        jdata = json.dumps(RemoteSSO.getData)
+        req = urllib.request.Request(self.url,
+                                     jdata.encode('utf-8'),
+                                     RemoteSSO.header)
+        response = urllib.request.urlopen(req)
+        data = response.read()
+        response.close()
+        data = json.loads(data.decode())
+        return data
+        
+    def showSSO(self, data):
+        print('-' * 20)
+        for i in data['MethodOutput']:
+            print('IP:', i['Ip'])
+            print('FriendlyName:', i['FriendlyName'])
+            print('-' * 20)
+
+    def delSSO(self, ip):
+        jdata = json.dumps(RemoteSSO.getData)
+        req = urllib.request.Request(self.url,
+                                     jdata.encode('utf-8'),
+                                     RemoteSSO.header)
+        response = urllib.request.urlopen(req)
+        data = response.read()
+        response.close()
+        data = data.decode()
+        #print(data)
+        data = json.loads(data)
+
+        for i in data['MethodOutput']:
+            if i['Ip'] == str(ip):
+                index = i['Index']
+        try:
+            RemoteSSO.delData['MethodInput']['Index'] = index
+        except:
+            return -1
+        else:
+            jdata = json.dumps(RemoteSSO.delData)
+            req = urllib.request.Request(self.url,
+                                         jdata.encode('utf-8'),
+                                         RemoteSSO.header)
+            response = urllib.request.urlopen(req)
+            data = response.read()
+            response.close()
+            data = data.decode()
+            #print(data)
+            data = json.loads(data)
+            if data['Message'] == 'success':
+                return (True, data)
+            else:
+                return (False, data)
+
+
+if __name__ == '__main__':
+#    alexSSO = SSO('http://10.103.64.44:12348')
+#    result = alexSSO.getSSO()
+#    alexSSO.showSSO(result)
+#    (result, data) = alexSSO.addSSO('6.6.7.8', '123456', 'test7')
+#    if result == True:
+#        print('adding successfully.')
+#        #print(data)
+#    alexSSO.showSSO(alexSSO.getSSO())
+
+    #(result, data) = alexSSO.delSSO('6.6.7.8')
+    #if result == 1:
+    #    print('deleting successfully.')
+    #alexSSO.showSSO(alexSSO.getSSO())
+
+    rmtSSO = RemoteSSO('http://10.103.64.44:12348')
+    #result, data = rmtSSO.addRemoteSSO('1.2.3.4', 2260, 'test1')
+    #print(result, data)
+
+    rmtSSO.showSSO((rmtSSO.getRemoteSSO()))
+    rmtSSO.delSSO('1.2.3.4')
