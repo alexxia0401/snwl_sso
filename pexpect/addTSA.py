82d0b9b1a182aa219c1b57f173aa4e86a0d2c132
Qing Xia <alexxia@outlook.com>
2017-05-09 10:59:45 +0800

    use argparse module

--- pexpect/addTSA.py
+++ pexpect/addTSA.py
@@ -2,40 +2,37 @@
 
 '''
 This script could automatically add TSA agent and enable it.
-Written by: Alex (Qing) Xia
-Version: 0.2
-Date: 3/22/2017
-This script doesn't work if UTM is not in non-conf t mode.
-!!! Using python3 !!!
+Written by: Qing Xia
 reference:
 https://pexpect.readthedocs.io/en/latest/api/pexpect.html#handling-unicode
-Tested on Ubuntu16.04
-apt-get install python-pip3
-pip3 install pexpect
+Tested on Ubuntu16.04 and Python3.5
 '''
 
+import argparse
 import pexpect
-import time
 import sys
-
-def usage():
-    print('''Usage: command WANIP TSAAgentIP sharedKey
-e.g. ./addSSO.py 10.0.0.20 192.168.10.10 123456''')
+import time
 
 def checkPara():
-    '''check parameters'''
-    while True:
-        if len(sys.argv) == 1:
-            usage()
-            sys.exit()
-        elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
-            usage()
-            sys.exit()
-        elif len(sys.argv) != 4: 
-            usage()
-            sys.exit()
-        else:
-            break
+    '''check input parameters'''
+    usage='''e.g. ./addTSA.py 10.0.0.20 192.168.10.10 123456'''
+    parser = argparse.ArgumentParser(description=usage)
+    parser.add_argument('wanip',
+                        help='firewall WAN IP, need ssh to be enabled')
+    parser.add_argument('ip', help='TSA agent IP')
+    parser.add_argument('key',
+                        help='TSA agent shared key',
+                        default=123456,
+                        nargs='?',
+                        type=str)
+    args = parser.parse_args()
+
+    global wanIp
+    global ip
+    global key
+    wanIp  = args.wanip
+    ip = args.ip
+    key = args.key
 
 def addTSA(wanip, tsaAgentIP, tsaAgentKey):
     '''add a new TSA on SonicOS'''
@@ -43,7 +40,10 @@ def addTSA(wanip, tsaAgentIP, tsaAgentKey):
     child.logfile = sys.stdout
     
     # first ssh login or not
-    index = child.expect(["\(yes/no\)\?", "admin@%s's password:" % wanip, "Password:", pexpect.TIMEOUT])
+    index = child.expect(["\(yes/no\)\?",
+        "admin@%s's password:" % wanip, "Password:",
+        pexpect.TIMEOUT])
+    
     if index == 0:
         child.sendline("yes")
     elif index == 1:
@@ -63,7 +63,10 @@ def addTSA(wanip, tsaAgentIP, tsaAgentKey):
     child.sendline("configure terminal")
 
     # override logged in admin user
-    index2 = child.expect(["\[no\]:", "config\([A-Z0-9]{12}\)#", pexpect.TIMEOUT])
+    index2 = child.expect(["\[no\]:",
+        "config\([A-Z0-9]{12}\)#",
+        pexpect.TIMEOUT])
+    
     if index2 == 0:
         child.sendline("yes")
     elif index2 == 1:
@@ -99,4 +102,4 @@ def addTSA(wanip, tsaAgentIP, tsaAgentKey):
 
 if __name__ == '__main__':
     checkPara()
-    addTSA(wanip=sys.argv[1], tsaAgentIP=sys.argv[2], tsaAgentKey=sys.argv[3])
+    addTSA(wanip=wanIp, tsaAgentIP=ip, tsaAgentKey=key)
