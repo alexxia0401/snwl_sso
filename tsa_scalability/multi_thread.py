9f892574f208623c38eba85568c20b1d2f9b5352
Qing Xia <alexxia@outlook.com>
2017-05-11 17:17:07 +0800

    py3

--- tsa_scalability/multi_thread.py
+++ tsa_scalability/multi_thread.py
@@ -3,6 +3,7 @@
 import http.client
 import socket
 import time
+import threading
 import urllib.request
 
 class BindableHTTPConnection(http.client.HTTPConnection):
@@ -21,27 +22,41 @@ def BindableHTTPConnectionFactory(source_ip):
         return bhc
     return _get
 
-page = 1
-ip = '192.168.81.1'
-
-while True:    
-    if page >= 14000:
-        page = 1
-        
+def visit(page, ip):
     class BindableHTTPHandler(urllib.request.HTTPHandler):
         def http_open(self, req):
             return self.do_open(BindableHTTPConnectionFactory(ip), req)
     
-    # connection counts within 1 second
-    for j in range(5):
-        try:
-            opener = urllib.request.build_opener(BindableHTTPHandler)
-            url = "http://10.103.64.220/page%s.htm" % str(page)
-            page += 1
-            print(opener.open(url).read().decode())
-            opener.close()
-        except Exception as e:
-            print('%s visit "%s" failed...' % (ip, url))
-            print(e)
-            continue   
-    time.sleep(1) 
+    try:
+        opener = urllib.request.build_opener(BindableHTTPHandler)
+        url = "http://10.103.64.220/page%s.htm" % str(page)
+        print(opener.open(url).read().decode())
+        opener.close()
+    except Exception as e:
+        print('%s visit %s failed...' % (ip, url))
+        print(e)
+
+d = 1
+ip = '192.168.81.%s' % str(d)
+page = 1
+
+threads = []
+
+for i in range(1, 256):
+    n = "t%s = threading.Thread(target=visit, args=(%s, '%s'))" % (str(i), str(page), ip)
+    #print n
+    exec(n)
+    d += 1
+    ip = '192.168.81.%s' % str(d)
+    page += 1
+    
+    m = "threads.append(t%s)" % str(i)
+    #print m
+    exec(m)   
+
+for t in threads:
+    t.setDaemon(True)
+    t.start()
+    
+t.join()
+time.sleep(1)
