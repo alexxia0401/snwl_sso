d7d7c7349c8543d6b312ba6693554db208bb7886
Qing Xia <alexxia@outlook.com>
2017-05-11 16:36:20 +0800

    add visit_single_ip.py

---
+++ tsa_scalability/visit_single_ip.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import http.client
+import socket
+import time
+import urllib.request
+
+class BindableHTTPConnection(http.client.HTTPConnection):
+    def connect(self):
+        """Connect to the host and port specified in __init__."""
+        self.sock = socket.socket()
+        self.sock.bind((self.source_ip, 0))
+        if isinstance(self.timeout, float):
+            self.sock.settimeout(self.timeout)
+        self.sock.connect((self.host,self.port))
+
+def BindableHTTPConnectionFactory(source_ip):
+    def _get(host, port=None, strict=None, timeout=0):
+        bhc=BindableHTTPConnection(host, port=port, timeout=timeout)
+        bhc.source_ip=source_ip
+        return bhc
+    return _get
+
+page = 1
+ip = '192.168.90.2'
+
+while True:    
+    if page >= 14000:
+        page = 1
+        
+    class BindableHTTPHandler(urllib.request.HTTPHandler):
+        def http_open(self, req):
+            return self.do_open(BindableHTTPConnectionFactory(ip), req)
+    
+    for j in range(5):
+        try:
+            opener = urllib.request.build_opener(BindableHTTPHandler)
+            url = "http://10.103.64.220/page" + str(page) + ".htm"
+            page += 1
+            print(opener.open(url).read().decode())
+            opener.close()
+        except Exception as e:
+            print('Connection "%s" failed...' % url)
+            print(e)
+            continue   
+    time.sleep(1) 
