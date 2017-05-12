e7989f4143439aff95b04b9384e06db00ff522c8
Qing Xia <alexxia@outlook.com>
2017-05-12 12:22:36 +0800

    rename

--- tsa_scalability/conf_ip_del.py
+++ tsa_scalability/conf_ip_del.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def conf_ip(c, ipRange):
+    '''c means 3rd ip range, ipRange means 4th MAX ip range
+    e..g conf_ip(1, 256) will generate 192.168.1.1 - 192.168.1.255'''
     filename = 'conf_ip_del_' + str(c) + '.bat'
     with open(filename, 'w') as fp:
         for i in range(1, ipRange):
             fp.write('netsh interface ipv4 delete address name="Local Area Connection" addr=192.168.%s.%s\r\n' % (c, i))
 
 if __name__ == '__main__':
-    for x in range(81, 90):
-        conf_ip(x, 63)
+    for i in range(81, 90):
+        conf_ip(i, 256)
