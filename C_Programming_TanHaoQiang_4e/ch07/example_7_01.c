877c05be66519fe4de53781241832ea7c4909fe7
alexxia <alexxia@outlook.com>
2017-07-20 00:07:32 +0800

    chapter 7 exp 1 & 2

---
+++ C_Programming_TanHaoQiang_4e/ch07/example_7_01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    void print_star();
+    void print_message();
+    print_star();
+    print_message();
+    print_star();
+    return 0;
+}
+
+void print_star()
+{
+    int i;
+    for(i = 0; i < 16; i++)
+        printf("*");
+    printf("\n");    
+}
+
+void print_message()
+{
+    printf("How do you do!\n");
+}
---
+++ C_Programming_TanHaoQiang_4e/ch07/example_7_02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int max(int, int);
+    int a, b, c;
+    printf("Please enter two integer numbers: ");
+    scanf("%d,%d", &a, &b);
+    c = max(a, b);
+    printf("max is %d\n", c);
+}
+
+
+int max(int x, int y)
+{
+    int z;
+    z = x > y? x: y;
+    return z;
+}
