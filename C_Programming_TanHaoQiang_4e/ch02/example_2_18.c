f235eb7421eb1534f8265719582d71d6e85dade9
alexxia <alexxia@outlook.com>
2017-07-25 22:25:02 +0800

    chapter 2

--- C_Programming_TanHaoQiang_4e/ch02/example_2_18.c
+++ C_Programming_TanHaoQiang_4e/ch02/example_2_18.c
@@ -5,8 +5,7 @@ int main()
     int i, t;
     t = 1;
     i = 2;
-    while (i <= 5)
-    {
+    while (i <= 5) {
         t = t * i;
         i = i + 1;
     }
--- C_Programming_TanHaoQiang_4e/ch02/example_2_19.c
+++ C_Programming_TanHaoQiang_4e/ch02/example_2_19.c
@@ -5,8 +5,7 @@ int main()
     int sign = 1;
     double deno = 2.0, sum = 1.0, term;
     
-    while (deno <= 100)
-    {
+    while (deno <= 100) {
         sign = -sign;
         term = sign / deno;
         sum = sum + term;
