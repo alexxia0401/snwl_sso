c33695e2fbfadf73facabb50fe75da272ccb0556
Qing Xia <alexxia@outlook.com>
2017-08-07 09:44:23 +0800

    use fgets() to replace gets()

--- C_Programming_TanHaoQiang_4e/ch06/example_6_08.c
+++ C_Programming_TanHaoQiang_4e/ch06/example_6_08.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
 int main()
 {
-    char string[81];
+    const int STR_LEN = 81;
+    char string[STR_LEN];
     int i, num = 0, word = 0;
     char c;
     /* gets() is dangerous and should not be used. */
-    gets(string);
+    /* gets(string); */
+    fgets(string, STR_LEN, stdin);
+    string[strlen(string) - 1] = '\0';
     for (i = 0; (c = string[i]) != '\0'; ++i) {
         if (c == ' ') {
             word = 0;
