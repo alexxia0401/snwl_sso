2d4195268bccfe18050bca5e12584580be1e52bf
Qing Xia <alexxia@outlook.com>
2019-02-14 08:05:06 +0800

    commit 2 files

---
+++ python3_tutorial/04_02_for.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Measure some strings:
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
+
+for w in words[:]:  # Loop over a slice copy of the entire list.
+    if len(w) > 6:
+        words.insert(0, w)
+
+print(words)
