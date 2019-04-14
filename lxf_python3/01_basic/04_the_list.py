2a081259b8c703c04048559f7ffc4dbcdc679817
Qing Xia <alexxia@outlook.com>
2019-04-15 07:05:31 +0800

    modify 04 to add more list operation examples

--- lxf_python3/01_basic/04_the_list.py
+++ lxf_python3/01_basic/04_the_list.py
@@ -10,3 +10,24 @@ print('classmates[2] =', classmates[2])
 print('classmates[-1] =', classmates[-1])
 print('classmates.pop() =', classmates.pop())
 print('classmates =', classmates)
+
+classmates.append('Alex')
+print("classmates.append('Alex')", classmates)
+classmates.insert(1, 'Peter')
+print("classmates.insert(1, 'Peter')", classmates)
+classmates.pop(0)
+print("classmates.pop(0)", classmates)
+classmates[2] = 'Lawfer'
+print(classmates)
+
+print("\n正序显示")
+for i in range(len(classmates)):
+    print(classmates[i])
+
+print("\n倒序显示")
+for i in range(-1, -(len(classmates)+1), -1):
+    print(classmates[i])
+
+print("\nprint 'php' element")
+s = ['python', 'java', ['asp', 'php'], 'scheme']
+print(s[2][1])
