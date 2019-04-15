08b728202fc600be2c75de16058647442d796cef
Qing Xia <alexxia@outlook.com>
2019-04-16 01:58:43 +0800

    add for and while loop examples

---
+++ lxf_python3/01_basic/08_do_while.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+# calculate 1+2+...+100
+sum = 0
+n = 1
+while n <= 100:
+    sum = sum + n
+    n = n + 1
+print(sum)
+
+# calculate 1*2*...*100
+acc = 1
+n = 1
+while n <= 100:
+    acc = acc * n
+    n = n + 1
+print(acc)
