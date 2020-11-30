3794a652200d73f93fbfae236a3b736967cfa087
Qing Xia <alexxia@outlook.com>
2020-11-30 06:23:50 +0000

    update unicode example

--- Core_Python_Programming_2e_py3/ch06/uniFile.py
+++ Core_Python_Programming_2e_py3/ch06/uniFile.py
@@ -6,14 +6,14 @@ Writes a Unicode string to a file in utf-8 and reads it back in.
 CODEC = 'utf-8'
 FILE = 'unicode.txt'
 
-hello_out = u'Hello World\n'
-#bytes_out = hello_out.encode(CODEC)
-f = open(FILE, 'w')
-f.write(hello_out)  #f.write(bytes_out)
+hello_out = 'Hello World\n'  # In py3, str is already unicode
+# bytes_out = hello_out.encode(CODEC)
+f = open(FILE, 'w', encoding=CODEC)  # In py3, no need to use encode() & decode(), just use encoding= when open file
+f.write(hello_out)
 f.close()
 
-f = open(FILE, 'r')
-bytes_in = f.read()
+f = open(FILE, 'r', encoding=CODEC)
+hello_in = f.read()
 f.close()
-#hello_in = bytes_in.decode(CODEC)
-print(bytes_in, end='')
+# hello_in = bytes_in.decode(CODEC)
+print(hello_in, end='')
