3179cf211b2bd56d6a8a4d23380bb7369c469c02
Qing Xia <alexxia@outlook.com>
2020-12-01 08:12:45 +0000

    Revert "re-run codenomicon failed cases."
    This reverts commit a23a62b3c52b44952d7233af01234e1c390051db.

--- UCTG/split_failed_cases_and_rerun.py
+++ UCTG/split_failed_cases_and_rerun.py
@@ -13,7 +13,6 @@ def random_half(l):
 failed_cases = list(range(1,300001))
 
 test_round = 1
-SPLIT_NUMBER = 20000
 # print("Failed test cases:", failed_cases)
 
 if len(failed_cases) <= 100:
@@ -23,13 +22,13 @@ else:
     test_round += 1
     to_run_cases = {}
     n = 0
-    if len(failed_cases) <= SPLIT_NUMBER:
+    if len(failed_cases) <= 20000:
         # no need to split, just run again
         to_run_cases[n] = failed_cases
     else:
-        while len(failed_cases) > SPLIT_NUMBER:
-            to_run_cases[n] = failed_cases[0:SPLIT_NUMBER]
-            del failed_cases[0:SPLIT_NUMBER]
+        while len(failed_cases) > 20000:
+            to_run_cases[n] = failed_cases[0:20000]
+            del failed_cases[0:20000]
             n += 1
         else:
             to_run_cases[n] = failed_cases
@@ -45,7 +44,7 @@ while test_round > 1:
         # print(i, to_run_cases[i])
         print(f"run round {i+1}")
         split_test_result = random_half(to_run_cases[i])
-        tmp_total_failed_cases.extend(split_test_result)  # equivalent to failed_cases
+        tmp_total_failed_cases.extend(split_test_result) # equiavelent to failed_cases
 
     # print(len(tmp_total_failed_cases))
     # test_round = -1
@@ -57,13 +56,13 @@ while test_round > 1:
         test_round += 1
         to_run_cases = {}
         n = 0
-        if len(tmp_total_failed_cases) <= SPLIT_NUMBER:
+        if len(tmp_total_failed_cases) <= 20000:
             # no need to split, just run again
             to_run_cases[n] = tmp_total_failed_cases
         else:
-            while len(tmp_total_failed_cases) >= SPLIT_NUMBER:
-                to_run_cases[n] = tmp_total_failed_cases[0:SPLIT_NUMBER]
-                del tmp_total_failed_cases[0:SPLIT_NUMBER]
+            while len(tmp_total_failed_cases) >= 20000:
+                to_run_cases[n] = tmp_total_failed_cases[0:20000]
+                del tmp_total_failed_cases[0:20000]
                 n += 1
             else:
                 to_run_cases[n] = tmp_total_failed_cases
